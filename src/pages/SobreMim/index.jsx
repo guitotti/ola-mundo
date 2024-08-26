fa12fcd0bff1da0987c2178c005b5e4a4df06df2
Guilherme Totti <dev.guilhermetotti@gmail.com>
2024-08-25 23:39:01 -0300

    feat: use PostModelo in SobreMim page

--- src/pages/SobreMim/index.jsx
+++ src/pages/SobreMim/index.jsx
@@ -1,8 +1,64 @@
+import PostModelo from "../../components/PostModelo";
+import styles from "./SobreMim.module.css";
+import fotoCapa from "../../assets/sobre_mim_capa.png";
+import fotoSobreMim from "../../assets/sobre_mim_foto.png";
+
 const SobreMim = () => {
   return (
-    <div>
-      <h1>Sobre mim!</h1>;
-    </div>
+    <PostModelo fotoCapa={fotoCapa} titulo="Sobre mim">
+      <h3 className={styles.subtitulo}>Oi, eu sou o Guilherme!</h3>
+
+      <img src={fotoSobreMim} className={styles.fotoSobreMim} />
+
+      <p className={styles.paragrafo}>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas quas
+        soluta maiores temporibus adipisci est! Error voluptatibus tempore
+        officiis, in assumenda omnis quasi, veritatis, exercitationem aliquid
+        provident corporis rerum dolore.
+      </p>
+      <p className={styles.paragrafo}>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas quas
+        soluta maiores temporibus adipisci est! Error voluptatibus tempore
+        officiis, in assumenda omnis quasi, veritatis, exercitationem aliquid
+        provident corporis rerum dolore.
+      </p>
+      <p className={styles.paragrafo}>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas quas
+        soluta maiores temporibus adipisci est! Error voluptatibus tempore
+        officiis, in assumenda omnis quasi, veritatis, exercitationem aliquid
+        provident corporis rerum dolore.
+      </p>
+      <p className={styles.paragrafo}>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas quas
+        soluta maiores temporibus adipisci est! Error voluptatibus tempore
+        officiis, in assumenda omnis quasi, veritatis, exercitationem aliquid
+        provident corporis rerum dolore.
+      </p>
+      <p className={styles.paragrafo}>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas quas
+        soluta maiores temporibus adipisci est! Error voluptatibus tempore
+        officiis, in assumenda omnis quasi, veritatis, exercitationem aliquid
+        provident corporis rerum dolore.
+      </p>
+      <p className={styles.paragrafo}>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas quas
+        soluta maiores temporibus adipisci est! Error voluptatibus tempore
+        officiis, in assumenda omnis quasi, veritatis, exercitationem aliquid
+        provident corporis rerum dolore.
+      </p>
+      <p className={styles.paragrafo}>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas quas
+        soluta maiores temporibus adipisci est! Error voluptatibus tempore
+        officiis, in assumenda omnis quasi, veritatis, exercitationem aliquid
+        provident corporis rerum dolore.
+      </p>
+      <p className={styles.paragrafo}>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas quas
+        soluta maiores temporibus adipisci est! Error voluptatibus tempore
+        officiis, in assumenda omnis quasi, veritatis, exercitationem aliquid
+        provident corporis rerum dolore.
+      </p>
+    </PostModelo>
   );
 };
 
