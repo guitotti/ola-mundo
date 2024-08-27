de8c685cff9fd6013788c1edf2b980ed3f8aab4c
Guilherme Totti <dev.guilhermetotti@gmail.com>
2024-08-26 23:25:21 -0300

    feat: add Link to PostCard

--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -1,19 +1,22 @@
 /* eslint-disable react/prop-types */
+import { Link } from "react-router-dom";
 import styles from "./Post.module.css";
 
 const PostCard = ({ post }) => {
   return (
-    <div className={styles.post}>
-      <img
-        className={styles.capa}
-        src={`/posts/${post.id}/capa.png`}
-        alt="Imagem de capa do post"
-      />
+    <Link to={`/posts/${post.id}`}>
+      <div className={styles.post}>
+        <img
+          className={styles.capa}
+          src={`/posts/${post.id}/capa.png`}
+          alt="Imagem de capa do post"
+        />
 
-      <h2 className={styles.titulo}>{post.titulo}</h2>
+        <h2 className={styles.titulo}>{post.titulo}</h2>
 
-      <button className={styles.botaoLer}>Ler</button>
-    </div>
+        <button className={styles.botaoLer}>Ler</button>
+      </div>
+    </Link>
   );
 };
 
