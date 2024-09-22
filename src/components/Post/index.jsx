e2305f626a738f82167ef6ed965691d7b677a47b
Guilherme Totti <dev.guilhermetotti@gmail.com>
2024-09-22 20:59:49 -0300

    feat: display styled Post text

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -1,11 +1,25 @@
+import './Post.css';
 import { useParams } from "react-router-dom";
+import posts from "../../json/posts.json";
+import PostModelo from "../PostModelo";
+import ReactMarkdown from "react-markdown";
 
 const Post = () => {
   const parametros = useParams();
-  
+
+  const post = posts.find((post) => {
+    return post.id === Number(parametros.id);
+  });
+
+  console.log(post);
+
   return (
-    <h1>Post {parametros.id}</h1>
-  )
+    <PostModelo fotoCapa={`/posts/${post.id}/capa.png`} titulo={post.titulo}>
+      <div className="post-markdown-container">
+        <ReactMarkdown>{post.texto}</ReactMarkdown>
+      </div>
+    </PostModelo>
+  );
 };
 
 export default Post;
